7fb4b472fbc78bfbfd98b54378d49dca91b84a5c
NegiSushant <sushantsinghnegi1998@gmail.com>
2025-09-17 18:00:58 +0530

    database setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/photo-1506744038136-46273834b3fb**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
