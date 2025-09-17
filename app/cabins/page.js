5803ca2040649a994cb9a1776389303748aee94a
NegiSushant <sushantsinghnegi1998@gmail.com>
2025-09-17 11:36:52 +0530

    first commit

---
+++ app/cabins/page.js
@@ -0,0 +1,19 @@
+import Counter from "../components/Counter";
+
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Cabin Page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.name}>{user.name}</li>
+        ))}
+      </ul>
+      <Counter users={data} />
+    </div>
+  );
+}
---
+++ app/components/Logo.js
@@ -0,0 +1,10 @@
+export default function Logo() {
+  return (
+    <a href="/" className="flex items-center gap-4 z-10">
+      <img src="/icon.png" height="60" width="60" alt="The Wild Oasis logo" />
+      <span className="text-xl font-semibold text-primary-100">
+        The WIld Oasis
+      </span>
+    </a>
+  );
+}
