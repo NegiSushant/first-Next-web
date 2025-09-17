f51437a43f6fdb7cb58751d8661a39386e0dea08
NegiSushant <sushantsinghnegi1998@gmail.com>
2025-09-17 17:16:54 +0530

    creating some pages

--- app/account/page.js
+++ app/account/page.js
@@ -1,3 +1,11 @@
+export const metadata = {
+  title: "Guest  area",
+};
+
 export default function Page() {
-  return <div>Hello from account Page</div>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, Jack
+    </h2>
+  );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,35 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import Logo from "@/app/_components/Logo";
+import Navigation from "@/app/_components/Navigation";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+import "@/app/_styles/global.css";
+import Header from "./_components/Header";
 
 export const metadata = {
-  title: "The Wild Oasis",
+  // title: "The Wild Oasis",
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "Welcome | The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the Nainital, Uttarakhand, surrounded by beautiful mountains, forest and lake.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>© 2025 The Wild Oasis</footer>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
